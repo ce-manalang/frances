ec45d13f0db0c8892fb7135b93e2ca8a12a5e38b
ce <cecillemanalang@gmail.com>
2025-04-29 17:52:44 +0800

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Raleway } from "next/font/google";
+import "./globals.css";
+
+const raleway = Raleway({
+  variable: "--font-raleway",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "texts and images by ce manalang",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${raleway.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,380 @@
+export default function Home() {
+  return (
+    <>
+      <div className="section top">
+        <div className="container">
+          <a className="button u-pull-right" href="#contact">
+            contact
+          </a>
+          <a className="button u-pull-right" href="#about">
+            about
+          </a>
+        </div>
+      </div>
+
+      <div className="section hero">
+        <div className="container">
+          <div className="row">
+            <div className="one-half column">
+              <h1 className="hero-heading">texts and images</h1>
+              <p>by ce manalang</p>
+              <a className="button" href="#readmore">
+                read more
+              </a>
+            </div>
+            <div className="one-half column phones">
+              <img
+                className="phone"
+                src="https://s3.us-east-2.amazonaws.com/cemanalang.com/images/where-hearts-meet-robots.png"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="section values" id="readmore">
+        <div className="container">
+          <h4 className="section-heading">books</h4>
+          <div className="row">
+            <div className="one-half column">
+              <a href="https://s3.us-east-2.amazonaws.com/cemanalang.com/images/love-letters-to-ruby.jpg">
+                <img
+                  className="u-max-full-width"
+                  src="https://s3.us-east-2.amazonaws.com/cemanalang.com/images/love-letters-to-ruby.jpg"
+                />
+              </a>
+              <h4 className="section-heading">love letters to ruby</h4>
+              <p>
+                <i>work in progress</i>. an illustrated retrospection of the
+                joys and pitfalls of web development
+              </p>
+            </div>
+            <div className="one-half column category">
+              <a href="https://s3.us-east-2.amazonaws.com/cemanalang.com/images/slaying-the-joy-snatchers.jpg">
+                <img
+                  className="u-max-full-width"
+                  src="https://s3.us-east-2.amazonaws.com/cemanalang.com/images/slaying-the-joy-snatchers.jpg"
+                />
+              </a>
+              <h4 className="section-heading">slaying the joy snatchers</h4>
+              <p>
+                daily challenges to win against negative thoughts and feelings
+              </p>
+              <a
+                className="button"
+                href="https://shop.omflit.com/collections/childrens-books/products/slaying-the-joy-snatchers"
+                target="_blank"
+              >
+                go to website
+              </a>
+            </div>
+          </div>
+
+          <h4 className="section-heading">events</h4>
+          <div className="row">
+            <div className="one-half column">
+              <a href="https://s3.us-east-2.amazonaws.com/cemanalang.com/images/things-that-fall.jpg">
+                <img
+                  className="u-max-full-width"
+                  src="https://s3.us-east-2.amazonaws.com/cemanalang.com/images/things-that-fall.jpg"
+                />
+              </a>
+              <h4 className="section-heading">things that fall</h4>
+              <p>for ang ink annual exhibit 2017: brave new world</p>
+              <a
+                className="button"
+                href="https://www.facebook.com/AngINK.org/photos/a.415499148273/10156280760688274/"
+                target="_blank"
+              >
+                see event details
+              </a>
+            </div>
+            <div className="one-half column category">
+              <a href="https://s3.us-east-2.amazonaws.com/cemanalang.com/images/conquering-your-fears.jpg">
+                <img
+                  className="u-max-full-width"
+                  src="https://s3.us-east-2.amazonaws.com/cemanalang.com/images/conquering-your-fears.jpg"
+                />
+              </a>
+              <h4 className="section-heading">conquering your fears</h4>
+              <p>for ang ink annual exhibit 2018: riddinkulus</p>
+              <a
+                className="button"
+                href="https://www.facebook.com/AngINK.org/photos/a.415499148273/10157393792363274/"
+                target="_blank"
+              >
+                see event details
+              </a>
+            </div>
+          </div>
+          <div className="row">
+            <div className="one-half column">
+              <a href="https://s3.us-east-2.amazonaws.com/cemanalang.com/images/the-world-needs-more-love-letters.jpg">
+                <img
+                  className="u-max-full-width"
+                  src="https://s3.us-east-2.amazonaws.com/cemanalang.com/images/the-world-needs-more-love-letters.jpg"
+                />
+              </a>
+              <h4 className="section-heading">
+                the world needs more love letters
+              </h4>
+              <p>
+                for ang ink annual exhibit 2019: <b>ink</b>wento: stories from
+                childhood
+              </p>
+              <a
+                className="button"
+                href="https://www.facebook.com/AngINK.org/posts/pfbid02LsJ658kJRfk6R9i4z6Zc7UzDyGG93GDeYerVFg94Yfz8ur7nzABwwY3a9JuBYadbl"
+                target="_blank"
+              >
+                see event details
+              </a>
+            </div>
+            <div className="one-half column category">
+              <a href="https://s3.us-east-2.amazonaws.com/cemanalang.com/images/dynamic-elements.jpg">
+                <img
+                  className="u-max-full-width"
+                  src="https://s3.us-east-2.amazonaws.com/cemanalang.com/images/dynamic-elements.jpg"
+                />
+              </a>
+              <h4 className="section-heading">lanthanium and erbium</h4>
+              <p>
+                for trial &amp; error exhibit 2019: dynamic elements: an
+                artistic take on the periodic table
+              </p>
+              <a
+                className="button"
+                href="https://www.facebook.com/hashtag/dynamicelements2019"
+                target="_blank"
+              >
+                see event details
+              </a>
+            </div>
+          </div>
+          <div className="row">
+            <div className="one-half column">
+              <a href="https://s3.us-east-2.amazonaws.com/cemanalang.com/images/some-earth-heroes.jpg">
+                <img
+                  className="u-max-full-width"
+                  src="https://s3.us-east-2.amazonaws.com/cemanalang.com/images/some-earth-heroes.jpg"
+                />
+              </a>
+              <h4 className="section-heading">some earth heroes</h4>
+              <p>
+                for ang ink annual exhibit 2020: kalikasan kabataan kinabukasan
+              </p>
+              <a
+                className="button"
+                href="https://www.facebook.com/AngINK.org/posts/pfbid02LyQn5FNaaDtSFdhLF5QySAtH3X41pby35NyK9rqEgYxh183AqPWF4fKvMhW7ut1al"
+                target="_blank"
+              >
+                see event details
+              </a>
+            </div>
+            <div className="one-half column category">
+              <a href="https://s3.us-east-2.amazonaws.com/cemanalang.com/images/input-creative-process-output.jpg">
+                <img
+                  className="u-max-full-width"
+                  src="https://s3.us-east-2.amazonaws.com/cemanalang.com/images/input-creative-process-output.jpg"
+                />
+              </a>
+              <h4 className="section-heading">
+                the creative process is like i/o streams
+              </h4>
+              <p>
+                for ang ink annual exhibit 2021: <b>ink</b> story: 30 years of
+                ang ilustrador ng kabataan
+              </p>
+              <a
+                className="button"
+                href="https://www.facebook.com/AngINK.org/posts/pfbid0MoMa2gEuQCHpXPn9e2yaWLR4GPhXTJe9uXx5V6Mh6H9KJ3ZgDiJGT4ME9QS4Ytuil"
+                target="_blank"
+              >
+                see event details
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="section categories">
+        <div className="container">
+          {/* <h4 className="section-heading">everything else</h4> */}
+          <div className="row">
+            <div className="one-half column category">
+              <h4 className="section-heading">centimentalcomics</h4>
+              <p>some comics about art and internet</p>
+              <a
+                className="button"
+                href="https://centimentalcomics.com"
+                target="_blank"
+              >
+                go to website
+              </a>
+							&nbsp;
+              <a
+                className="button"
+                href="https://redbubble.com/people/mxtpslvrsrnthng/shop"
+                target="_blank"
+              >
+                go to poster shop
+              </a>
+            </div>
+            <div className="one-half column">
+              <a href="https://s3.us-east-2.amazonaws.com/cemanalang.com/images/where-do-the-unhugged-hugs-go.jpg">
+                <img
+                  className="u-max-full-width"
+                  src="https://s3.us-east-2.amazonaws.com/cemanalang.com/images/where-do-the-unhugged-hugs-go.jpg"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="section categories" id="about">
+        <div className="container">
+          <div className="row">
+            <div className="one-half column">
+              <a href="https://s3.us-east-2.amazonaws.com/cemanalang.com/images/ce4.jpg">
+                <img
+                  className="u-max-full-width"
+                  src="https://s3.us-east-2.amazonaws.com/cemanalang.com/images/ce4.jpg"
+                />
+              </a>
+            </div>
+            <div className="one-half column u-text-left">
+              <h4 className="section-heading">about</h4>
+              <p>ce manalang is a content &amp; web developer.</p>
+              <p>
+                she grew up in bulacan with her two older siblings and their
+                collection of books. she studied information technology at
+                university of santo tomas. she drew comics on paper and
+                submitted some of them to <a
+                  href="https://www.ang-ink.org/illustrators/ce-manalang"
+                  target="_blank"
+                >ang ilustrador ng kabataan</a> in 2017.
+              </p>
+              <p>
+                early that year she was accepted and started to participate in
+                the org&apos;s annual exhibit: things that fall (2017), conquering
+                your fears (2018), the world needs more love letters (2019),
+                some earth heroes (2020), the creative process is like an i/o
+                streams (2021). it was there that the editor of <a
+                  href="https://shop.omflit.com/collections/childrens-books/products/slaying-the-joy-snatchers"
+                  target="_blank"
+                >slaying the joy snatchers</a> (2021) found her and commissioned her to work on the book&apos;s
+                illustrations. she also participated in trial &amp; error
+                collective&apos;s exhibit: <a
+                  href="https://www.facebook.com/hashtag/dynamicelements2019"
+                  target="_blank"
+                >dynamic elements</a> in 2019 with artworks: lanthanum, and erbium.
+              </p>
+              <p>
+                she later moved her comics to <a href="https://centimentalcomics.com" target="_blank">
+                  centimentalcomics.com</a>.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="section get-help" id="contact">
+        <div className="container">
+          <div className="row">
+            <h3 className="section-heading">contact</h3>
+            <p>
+              <a href="mailto:cecillemanalang@gmail.com">
+                cecillemanalang@gmail.com
+              </a>
+            </p>
+          </div>
+          <div className="row">
+            <p>
+              <a
+                className="button docs-button docs-button-share docs-bg-storygraph"
+                href="https://app.thestorygraph.com/profile/_ce"
+                target="_blank"
+              >
+                storygraph
+              </a>
+              <a
+                className="button docs-button docs-button-share docs-bg-instagram"
+                href="https://instagram.com/ce_walalang"
+                target="_blank"
+              >
+                instagram
+              </a>
+              <a
+                className="button docs-button docs-button-share docs-bg-mastodon"
+                href="https://bookstodon.thestorygraph.com/@ce_"
+                target="_blank"
+              >
+                mastodon
+              </a>
+              <a
+                className="button docs-button docs-button-share docs-bg-facebook"
+                href="https://facebook.com/ce.walalang"
+                target="_blank"
+              >
+                facebook
+              </a>
+              <a
+                className="button docs-button docs-button-share docs-bg-reddit"
+                href="https://reddit.com/user/ce-walalang"
+                target="_blank"
+              >
+                reddit
+              </a>
+              <a
+                className="button docs-button docs-button-share docs-bg-medium"
+                href="https://medium.com/ce-walalang"
+                target="_blank"
+              >
+                medium
+              </a>
+              <a
+                className="button docs-button docs-button-share docs-bg-github"
+                href="https://github.com/ce-walalang"
+                target="_blank"
+              >
+                github
+              </a>
+              <a
+                className="button docs-button docs-button-share docs-bg-dribbble"
+                href="https://dribbble.com/ce-walalang"
+                target="_blank"
+              >
+                dribbble
+              </a>
+              <a
+                className="button docs-button docs-button-share docs-bg-linkedin"
+                href="https://linkedin.com/in/ce-walalang/"
+                target="_blank"
+              >
+                linkedin
+              </a>
+              <a
+                className="button docs-button docs-button-share docs-bg-devto"
+                href="https://dev.to/ce_walalang"
+                target="_blank"
+              >
+                devto
+              </a>
+              <a
+                className="button docs-button docs-button-share docs-bg-tiktok"
+                href="https://tiktok.com/@ce_walalang"
+                target="_blank"
+              >
+                tiktok
+              </a>
+            </p>
+          </div>
+          <div className="row">
+            <p>© ce manalang 2023</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
